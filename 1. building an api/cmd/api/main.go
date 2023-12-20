f8330fd6d6960de7cd9cb802cadb91f6dc3a1b3a
aryskaliyev <aryskaliyev@nu.edu.kz>
2023-12-20 06:02:24 +0600

    working on api

--- 1. building an api/cmd/api/main.go
+++ 1. building an api/cmd/api/main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
-	
+
 	"github.com/go-chi/chi"
-	// "lincoln.boris/internal/handlers"
+	"lincoln.boris/internal/handlers"
 	log "github.com/sirupsen/logrus"
 )
 
+func main() {
+	log.SetReportCaller(true)
+	var r *chi.Mux = chi.NewRouter()
+	handlers.Handler(r)
 
+	fmt.Println("Starting GO API service...")
+
+	fmt.Println(`
+ ______     ______        ______     ______   __
+/\  ___\   /\  __ \      /\  __ \   /\  == \ /\ \
+\ \ \__ \  \ \ \/\ \     \ \  __ \  \ \  _-/ \ \ \
+ \ \_____\  \ \_____\     \ \_\ \_\  \ \_\    \ \_\
+  \/_____/   \/_____/      \/_/\/_/   \/_/     \/_/ `)
+
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
+}
