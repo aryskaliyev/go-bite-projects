6e2422878e9ef21465e4f88e3e7dae91021cb549
aryskaliyev <aryskaliyev@nu.edu.kz>
2024-01-27 21:29:23 +0600

    Working on heartbeats

--- 4. concurrency in go/main.go
+++ 4. concurrency in go/main.go
@@ -5,24 +5,11 @@ import (
 	"time"
 )
 
-func reallyLongCalculation(value interface{}) {
-	time.Sleep(2 * time.Second)
-	fmt.Println("[+] Really long calculation executed!")
-}
-
 func main() {
-	var value interface{}
-	select {
-	case <-done:
-		return
-	case value = <-valueStream:
-	}
+	ticker := time.Tick(1 * time.Second)
 
-	result := reallyLongCalculation(value)
-
-	select {
-	case <-done:
-		return
-	case resultStream<- result:
+	for i := 0; i < 50; i++ {
+		tickTime := <-ticker
+		fmt.Println("Tick at", tickTime)
 	}
 }
